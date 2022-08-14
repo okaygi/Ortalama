4c25a25fa5297e429947885c28b05fdd486ff409
okaygi <ozcankaygi@hotmail.com>
2022-08-14 13:47:52 +0300

    deneme

---
+++ Ortalama/Ortalama.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+
+public class Ortalama {
+public static void main(String[] args) {
+
+    int mat, fiz, kim, turk, tar, muz;
+    
+
+    Scanner inp = new Scanner(System.in);
+
+    System.out.println("Matematik giriş: ");
+     mat = inp.nextInt( );
+     System.out.println("Fizik giriş: ");
+     fiz = inp.nextInt( );
+     System.out.println("Kimya giriş: ");
+     kim = inp.nextInt( );
+     System.out.println("Türkçe giriş: ");
+     turk = inp.nextInt( );
+     System.out.println("Tarih giriş: ");
+     tar = inp.nextInt( );
+     System.out.println("Müzik giriş: ");
+     muz = inp.nextInt( );
+     int toplam = (mat+fiz+kim+turk+tar+muz)/6;
+
+    boolean ortalama = toplam >60;
+
+     System.out.println(ortalama == true ? "Sınıfı Geçti" : "Sınıfta Kaldı");
+
+
+}
+}
